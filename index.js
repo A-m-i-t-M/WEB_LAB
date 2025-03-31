23c7b062b2cd4d39d71b57647dd3d8d11c6baa90
Amit Madhbahavi <154544727+A-m-i-t-M@users.noreply.github.com>
2025-04-01 01:39:53 +0530

    Q1-11 & 13

--- index.js
+++ index.js
@@ -55,3 +55,73 @@ function fifth(arr){
 }
 console.log("Fifth program");
 fifth([1,2,3,4,5]);
+
+
+function sixth(arr) {
+    return arr.reduce((total, num) => total * num, 1);
+}
+console.log("SUM",sum([1,2,3,4,5,6]));
+console.log("PROD",sixth([1,2,3,4,5,6]));
+
+function seventh(str) {
+    return str.split('').reverse().join('');
+}
+console.log("Reverse string of akagemakill is ",seventh('akagemakill'));
+
+var eighth = {
+    "merry": "god",
+    "christmas": "jul",
+    "and": "och",
+    "happy": "gott",
+    "new": "nytt",
+    "year": "år"
+};
+
+function translateChristmasCard(cardText) {
+    var words = cardText.split(' ');
+    var translatedWords = words.map(function(word) {
+        return eighth[word.toLowerCase()] || word;
+    });
+    return translatedWords.join(' ');
+}
+console.log(translateChristmasCard("Merry Christmas and Happy New Year"));
+
+
+function ninth(words) {
+    let longest = words.reduce((longest, current) => {
+        return current.length > longest.length ? current : longest;
+    }, "");
+    return longest.length;
+}
+console.log(ninth(["marde","mereko","koi","fayda","nhi","jeene","ka"]));
+
+
+function tenth(words, length) {
+    return words.filter(word => word.length > length);
+}
+console.log(tenth(["marde","mereko","koi","fayda","nhi","jeene","ka"],4));
+
+
+function eleventh(str) {
+    var freq = {};
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i];
+        if (freq[char]) {
+            freq[char]++;
+        } else {
+            freq[char] = 1;
+        }
+    }
+    return freq;
+}
+console.log(eleventh("commit karde bhai"));
+
+console.log("This is the thirteenth program");
+var names = [];
+var name;
+while (name = prompt("Enter a name (or cancel to stop):")) {
+    names.push(name);
+}
+
+names.sort();
+alert("Sorted names: \n" + names.join("\n"));
